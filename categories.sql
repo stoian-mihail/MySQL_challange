2577b4fe187a90d352cc97a405805f962b9fac26
stoian-mihail <stoian.mihail.alexandru@gmail.com>
2019-09-09 15:15:32 +0300

    Create table querries manipulate table querries -green populate tables with mock up data -green

---
+++ categories.sql
@@ -0,0 +1,9 @@
+
+INSERT INTO categories(category_name) VALUES ('Tools');
+INSERT INTO categories(category_name) VALUES ('Books');
+INSERT INTO categories(category_name) VALUES ('PC');
+INSERT INTO categories(category_name) VALUES ('Monitors');
+INSERT INTO categories(category_name) VALUES ('Food');
+INSERT INTO categories(category_name) VALUES ('Cars');
+INSERT INTO categories(category_name) VALUES ('Air Cond');
+INSERT INTO categories(category_name) VALUES ('Refrigerators');
