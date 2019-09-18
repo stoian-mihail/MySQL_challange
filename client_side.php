6cc9884e5a6763bce055b565d32af88cb754f983
stoian-mihail <stoian.mihail.alexandru@gmail.com>
2019-09-18 18:12:51 +0300

    Php functions implemented

---
+++ client_side.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <p>muie ba</p>
+    <form action="client_side.php" method ="post">
+        <input type="text" name="retrieve_customerIDa">
+        <input type="submit" value="find">
+    </form>
+    <?php
+    echo "string"; ?>
+  </body>
+</html>
